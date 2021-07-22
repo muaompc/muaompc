88e0d2344238836bf79245c23f40b82aa9cf942d
P. Zometa <zometa@anixet.com>
2021-07-22 15:30:19 +0200

    Update cython templates to use setuptools

--- muaompc/_ldt/codegen/former/cvp/tmpl/cython/tmpl_cvpsetup.py
+++ muaompc/_ldt/codegen/former/cvp/tmpl/cython/tmpl_cvpsetup.py
@@ -1,12 +1,11 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import Extension
 
 ext_modules = [Extension("{prefix}cvp",
-    ["src/cython/{prefix}cvp.pyx", "src/cython/{prefix}Ccvp.pyx",
+    ["src/cython/{prefix}cvp.pyx", 
         "src/{prefix}cvp.c",
-    "src/{prefix}dynmem.c",
-    "src/{prefix}cvpdynmem.c",
-    "src/cjson.c",  "src/{prefix}mtxops.c"],
+        "src/{prefix}dynmem.c",
+        "src/{prefix}cvpdynmem.c",
+        "src/cjson.c",  "src/{prefix}mtxops.c"],
     include_dirs = ["src/include"], library_dirs = ["src"],
     libraries=["m"]),
     Extension("{prefix}Ccvp",
--- muaompc/_ldt/codegen/mpc/tmpl/tmpl_setup.py
+++ muaompc/_ldt/codegen/mpc/tmpl/tmpl_setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
 from sys import path
 
 from Cython.Build import cythonize
@@ -7,7 +6,8 @@ from Cython.Build import cythonize
 path.append('./src/cython/')
 from {prefix}{former}setup import ext_modules as former_ext_modules
 from {prefix}{solver}setup import ext_modules as solver_ext_modules
-ext_modules = former_ext_modules + solver_ext_modules
+from {prefix}ctlsetup import ext_modules as ctl_ext_modules
+ext_modules = former_ext_modules + solver_ext_modules + ctl_ext_modules
 
 setup(name='{prefix}', packages=['{prefix}'],
         ext_package='{prefix}',
