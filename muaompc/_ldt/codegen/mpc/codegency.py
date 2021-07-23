48048a15d1c325a73f05d3236ea522c2fe5fe384
P. Zometa <zometa@anixet.com>
2021-07-23 11:25:44 +0200

    Fix generation of ctl cython setup for ALM+FGM solver

--- muaompc/_ldt/codegen/mpc/codegency.py
+++ muaompc/_ldt/codegen/mpc/codegency.py
@@ -1,8 +1,9 @@
 """Generate Cython interface for C code for the controller."""
+from muaompc._ldt import codegen
 import shutil
 import os
 
-from muaompc._ldt.codegen.codegen import BaseCodeGenerator as BCG
+from muaompc._ldt.codegen.codegen import codegen_doc
 from muaompc._ldt.codegen.mpc.codegen import CCodeGenerator as MCG
 
 
@@ -15,14 +16,32 @@ class CythonCodeGenerator(MCG, object):
         self.solver = mcg.solver
 
     def generate_code(self):
-        self._generate_cython_code()
+        if self.solver == 'fgm':
+            d = self._generate_dict_fgm()
+        elif self.solver == 'alm':
+            d = self._generate_dict_alm()
+        else:
+            raise TypeError
 
-    def _generate_cython_code(self):
-        d = dict(prefix=self.prefix,
+        self._generate_cython_code(d)
+
+    def _generate_dict_fgm(self):
+        return dict(prefix=self.prefix,
                    PREFIX=self.prefix.upper(),
                    former=self.former,
                    solver=self.solver,
+                   solver_dep="",
+                   doc = codegen_doc
         )
+
+    def _generate_dict_alm(self):
+        # code gen of ALM is identical to FGM, except that now FGM files are a depency
+        d = self._generate_dict_fgm()
+        solver_dep = '"src/{prefix}{solver}dynmem.c","src/{prefix}{solver}.c",'
+        d['solver_dep'] = solver_dep.format(**dict(prefix=self.prefix, solver='fgm'))
+        return d
+
+    def _generate_cython_code(self, d):
         self._replace_dict(d, self.prefix, 'ctlsetup.py', srcdir='cython')
         self._replace_dict(d, self.prefix, 'ctl.pyx', srcdir='cython')
         self._replace_dict(d, self.prefix, 'Cctl.pxd', srcdir='cython')
--- muaompc/_ldt/codegen/mpc/tmpl/cython/tmpl_ctlsetup.py
+++ muaompc/_ldt/codegen/mpc/tmpl/cython/tmpl_ctlsetup.py
@@ -1,9 +1,12 @@
+"""{doc}
+"""
+
 from setuptools import Extension
 
 ext_modules = [Extension("{prefix}ctl",
     ["src/cython/{prefix}ctl.pyx",
         "src/{prefix}ctldynmem.c","src/{prefix}ctl.c",
-        "src/{prefix}{solver}dynmem.c","src/{prefix}{solver}.c",
+        "src/{prefix}{solver}dynmem.c","src/{prefix}{solver}.c",{solver_dep}
         "src/{prefix}{former}dynmem.c","src/{prefix}{former}.c",
         "src/cjson.c", "src/{prefix}dynmem.c",
         "src/{prefix}mtxops.c"],
