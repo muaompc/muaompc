d7026f6a8d98658465b03c65cdd7a5d5d740d9c3
P. Zometa <zometa@anixet.com>
2021-08-09 09:10:42 +0200

    Update conf.py and .gitignore

--- doc/conf.py
+++ doc/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 import muaompc
 #rom muaompc import version as muaompc_version
 project = 'muAO-MPC'
-copyright = '2012-2016, Pablo Zometa'
+copyright = '2012-2021, Pablo Zometa'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
