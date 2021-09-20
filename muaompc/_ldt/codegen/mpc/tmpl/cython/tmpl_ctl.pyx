e4f588e4856d3def72919dd8e00d12dcb40c9ad4
P. Zometa <zometa@anixet.com>
2021-09-20 09:44:36 +0200

    Add access to condensed problem in MPC Cython iface.

--- muaompc/_ldt/codegen/mpc/tmpl/cython/tmpl_ctl.pyx
+++ muaompc/_ldt/codegen/mpc/tmpl/cython/tmpl_ctl.pyx
@@ -9,6 +9,21 @@ cimport {prefix}C{former} as C{former}
 cimport {prefix}C{solver} as C{solver}
 cimport {prefix}Cctl as Cctl
 
+cdef class Prb(object):
+    cdef Ccvp.{prefix}_cvp_prb *prb
+{prb_class}
+    def __cinit__(self, uint64_t prb):
+        self.prb = <C{former}.{prefix}_{former}_prb *> prb
+        self._update()
+
+    def _update(self):
+{prb_init}
+{prb_property}
+    def _set_property(self, uint64_t term):
+        t = <C{former}.{prefix}_term *> term
+        cdef cvarray cva = <double[:t.rows*t.cols]>t.data
+        return (array(cva[:]), t.rows, t.cols)
+
 cdef class Conf(object):
     cdef C{solver}.{prefix}_{solver}_conf *conf
 
@@ -18,7 +33,7 @@ cdef class Conf(object):
 
 cdef class Parameters(object):
     cdef C{former}.{prefix}_{former} *former
-{par_class}    
+{par_class}
     def __cinit__(self, uint64_t former):
         self.former = <C{former}.{prefix}_{former} *> former
 {par_init}
@@ -43,6 +58,7 @@ cdef class Ctl(object):
     cdef Cctl.{prefix}_ctl *ctl
     cdef Conf conf
     cdef Parameters parameters
+    cdef Prb prb
     cdef cvarray u_opt
 
     def __cinit__(self):
@@ -66,6 +82,10 @@ cdef class Ctl(object):
         def __get__(self):
             return self.parameters
 
+    property prb:
+        def __get__(self):
+            return self.prb
+
     def __init__(self, fname):
         fname = fname.encode()  # python 3, char *
         Cctl.{prefix}_ctl_setup_ctl(self.ctl, fname)
@@ -75,9 +95,12 @@ cdef class Ctl(object):
         self.conf = Conf(<uint64_t> conf)
         cdef C{former}.{prefix}_{former} *former = self.ctl.former
         self.parameters = Parameters(<uint64_t> former)
+        prb = <C{former}.{prefix}_{former}_prb *> former.prb
+        self.prb = Prb(<uint64_t> prb)
 
     def solve_problem(self):
         self.parameters._update()
         Cctl.{prefix}_ctl_solve_problem(self.ctl)
         n = self.ctl.solver.optvar_seqlen
         self.u_opt = <double[:n]> self.ctl.u_opt
+        self.prb._update()
