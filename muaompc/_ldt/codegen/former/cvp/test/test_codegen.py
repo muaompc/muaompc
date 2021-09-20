9448fac600a4289da49985caa1de5945ab98aa65
P. Zometa <zometa@anixet.com>
2021-09-20 10:05:26 +0200

    Format CVP codegen test

--- muaompc/_ldt/codegen/former/cvp/test/test_codegen.py
+++ muaompc/_ldt/codegen/former/cvp/test/test_codegen.py
@@ -29,8 +29,9 @@ class TestConstantHessian(unittest.TestCase):
         self.base_dir = os.path.dirname(__file__)
 
         self.paramref = dict(x_k=np.array([2, 3]),
-                xr=[np.array([5, 7]), np.array([5, 7]), np.array([11, 13])],
-                ur=[np.array([17]), np.array([19])])
+                             xr=[np.array([5, 7]), np.array(
+                                 [5, 7]), np.array([11, 13])],
+                             ur=[np.array([17]), np.array([19])])
 
         self.paramori = dict(x_k=np.array([2, 3]))
 
@@ -59,30 +60,31 @@ class TestConstantHessian(unittest.TestCase):
     @unittest.skip('This is how you skip')
     def test_socc(self):
         prefix = 'stocmpc'
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         g = 2*np.array([[0.33038257],
-               [0.3054006]])
+                        [0.3054006]])
         V = np.array([[0.,  0.],
-                    [0.10501746,  0.],
-                    [0.01899904,  0.02095512]])
-        Wm0 = np.array([[  9.67483607e-05,   0.00000000e+00],
-                   [  1.90325164e-02,   0.00000000e+00]])
-        wn0 = np.array([[ 2.02854877],
-                   [ 2.71451225]])
-        wvT0 = np.array([[ -9.67483607e-05,   0.00000000e+00]])
+                      [0.10501746,  0.],
+                      [0.01899904,  0.02095512]])
+        Wm0 = np.array([[9.67483607e-05,   0.00000000e+00],
+                        [1.90325164e-02,   0.00000000e+00]])
+        wn0 = np.array([[2.02854877],
+                        [2.71451225]])
+        wvT0 = np.array([[-9.67483607e-05,   0.00000000e+00]])
         ws0 = np.array([[-2.37754877]])
-        Wm1 = np.array([[  2.77866701e-04,   9.67483607e-05],
-                   [  1.72213330e-02,   1.90325164e-02]]) 
-        wn1 = np.array([[ 2.05438077],
-                   [ 2.45619226]])
-        wvT1 = np.array([[ -2.77866701e-04,  -9.67483607e-05]])
+        Wm1 = np.array([[2.77866701e-04,   9.67483607e-05],
+                        [1.72213330e-02,   1.90325164e-02]])
+        wn1 = np.array([[2.05438077],
+                        [2.45619226]])
+        wvT1 = np.array([[-2.77866701e-04,  -9.67483607e-05]])
         ws1 = np.array([[-2.40338077]])
 
         u_ub = np.array([[100], [100]])
-        v_ub = np.array([[-22.5], [-21.02973811], [ -2.61268764]])
+        v_ub = np.array([[-22.5], [-21.02973811], [-2.61268764]])
 
-        ccg = commoncodegen.CCodeGenerator(self.prbnamestoc, prefix, self.destdir)
+        ccg = commoncodegen.CCodeGenerator(
+            self.prbnamestoc, prefix, self.destdir)
         ccg.generate_code('float64_t')
         cdg = codegen.CCodeGenerator(self.cvpstoc, ccg)
         cdg.generate_code()
@@ -95,7 +97,7 @@ class TestConstantHessian(unittest.TestCase):
         path.append('./src/cython/')
         cvpsetup = __import__(prefix+'cvpsetup')
         setup(ext_modules=cythonize(cvpsetup.ext_modules),
-                script_args=['build_ext', '--inplace'])
+              script_args=['build_ext', '--inplace'])
         import stocmpccvp
         shutil.os.chdir('../../')
         qpx = stocmpccvp.Former()
@@ -108,48 +110,48 @@ class TestConstantHessian(unittest.TestCase):
         qp = qpx.form_problem(pardata)
 
         assert_allclose(qp['H'], H, rtol=1e-6,
-                err_msg='Hessian matrix')
+                        err_msg='Hessian matrix')
 
         assert_allclose(qp['g'], g, rtol=1e-6,
-                err_msg='gradient vector')
+                        err_msg='gradient vector')
 
         assert_allclose(qp['u_ub'], u_ub, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][0]['Wm'], Wm0, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][0]['wvT'], wvT0, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][0]['wn'], wn0, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][0]['ws'], ws0, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][1]['Wm'], Wm1, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][1]['wvT'], wvT1, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][1]['wn'], wn1, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['socc'][1]['ws'], ws1, rtol=1e-6,
-                err_msg='Constraint matrix')
-
+                        err_msg='Constraint matrix')
 
     #unittest.skip('This is how you skip')
+
     def test_H_mtx(self):
-# the original H was computed by muaompc 0.3.0, which uses half of the cost
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        # the original H was computed by muaompc 0.3.0, which uses half of the cost
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         prb = self.cvpref.form_condensed_prb(self.num)
         assert_allclose(prb['pmetric']['H']['fac0'], H, rtol=1e-6,
-                err_msg='Hessian matrix')
+                        err_msg='Hessian matrix')
 
     #unittest.skip('This is how you skip')
     def test_inc(self):
         prefix = 'mpcinc'
         prbname = 'regrefmpc'
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         g = 2*np.array([[-18.28409196],
-               [-20.31826494]])
+                        [-20.31826494]])
 
         cvpcy = self._get_cython_former(prbname, prefix)
 
@@ -161,24 +163,25 @@ class TestConstantHessian(unittest.TestCase):
         qp = cvpcy.form_problem(pardata)
 
         assert_allclose(qp['H'], H, rtol=1e-6,
-                err_msg='Hessian matrix')
+                        err_msg='Hessian matrix')
 
         assert_allclose(qp['g'], g, rtol=1e-6,
-                err_msg='gradient vector')
+                        err_msg='gradient vector')
 
     #unittest.skip('This is how you skip')
     def test_stc(self):
         prefix = 'mpcstc'
         prbname = 'regorigmpc'
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         g = 2*np.array([[0.33038257],
-               [0.3054006]])
+                        [0.3054006]])
         V = np.array([[0.,  0.],
-                    [0.10501746,  0.],
-                    [0.01899904,  0.02095512]])
+            [0.10501746,  0.],
+            [0.01899904,  0.02095512]])
         u_ub = np.array([[100], [100]])
-        v_ub = np.array([[-22.5], [-21.02973811], [ -2.61268764]])
+        v_ub = np.array([[-22.5],
+            [-21.02973811], [-2.61268764]])
 
         cvpcy = self._get_cython_former(prbname, prefix)
 
@@ -188,17 +191,17 @@ class TestConstantHessian(unittest.TestCase):
         qp = cvpcy.form_problem(pardata)
 
         assert_allclose(qp['H'], H, rtol=1e-6,
-                err_msg='Hessian matrix')
+                        err_msg='Hessian matrix')
 
         assert_allclose(qp['g'], g, rtol=1e-6,
-                err_msg='gradient vector')
+                        err_msg='gradient vector')
 
         assert_allclose(qp['V'], V, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['u_ub'], u_ub, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
         assert_allclose(qp['v_ub'], v_ub, rtol=1e-6,
-                err_msg='Constraint matrix')
+                        err_msg='Constraint matrix')
 
     @unittest.skip('This is how you skip')
     def test_outref(self):
@@ -206,10 +209,10 @@ class TestConstantHessian(unittest.TestCase):
         set_trace()
         prefix = 'mpcoutref'
         prbname = 'regoutrefmpc'
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         g = 2*np.array([[-18.28409196],
-               [-20.31826494]])
+                        [-20.31826494]])
 
         self.num.update(Cx=np.eye(2))
         cvpcy = self._get_cython_former(prbname, prefix)
@@ -221,11 +224,10 @@ class TestConstantHessian(unittest.TestCase):
         qp = cvpcy.form_problem(pardata)
 
         assert_allclose(qp['H'], H, rtol=1e-6,
-                err_msg='Hessian matrix')
+                        err_msg='Hessian matrix')
 
         assert_allclose(qp['g'], g, rtol=1e-6,
-                err_msg='gradient vector')
-
+                        err_msg='gradient vector')
 
     def _vecseq2array(self, vecseq):
         outer_rows = len(vecseq)
@@ -233,7 +235,7 @@ class TestConstantHessian(unittest.TestCase):
         arr = np.zeros((n*outer_rows, 1))
 
         for i, mtx in enumerate(vecseq):
-                arr[n*i:n*(i+1), 0] = mtx
+            arr[n*i:n*(i+1), 0] = mtx
 
         return arr
 
@@ -264,7 +266,7 @@ class TestConstantHessian(unittest.TestCase):
         path.append('./src/cython/')
         cvpsetup = import_module(prefix+'cvpsetup')
         setup(ext_modules=cythonize(cvpsetup.ext_modules),
-                script_args=['build_ext', '--inplace'])
+              script_args=['build_ext', '--inplace'])
         return
 
     def _get_cython_former(self, prbname, prefix):
@@ -274,7 +276,7 @@ class TestConstantHessian(unittest.TestCase):
         print(testdir)
         shutil.os.chdir(testdir)
         self._compile_cython_interface(prbname, prefix)
-        
+
         #print('\nCurrent path: ', shutil.os.path.abspath(shutil.os.curdir))
         path.append(testdir)
         modname = prefix + 'cvp'
@@ -287,10 +289,10 @@ class TestConstantHessian(unittest.TestCase):
     @unittest.skip('This is how you skip')
     def test_mex(self):
         prefix = 'mpcinc'
-        H = 2*np.array([[ 1.00157741, 0.00133198],
-            [0.00133198, 1.00146001]])
+        H = 2*np.array([[1.00157741, 0.00133198],
+                        [0.00133198, 1.00146001]])
         g = 2*np.array([[-18.28409196],
-               [-20.31826494]])
+                        [-20.31826494]])
 
         ccg = commoncodegen.CCodeGenerator(prefix, '.')
         ccg.generate_code('float64_t')
@@ -298,6 +300,7 @@ class TestConstantHessian(unittest.TestCase):
         cdg.generate_code()
         mexcdg = codegenmex.MatlabCodeGenerator(cdg)
         mexcdg.generate_code()
+
 
 if __name__ == '__main__':
     unittest.main()
